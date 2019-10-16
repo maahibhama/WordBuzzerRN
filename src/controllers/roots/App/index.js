5aeb89a061cff89de8cb3be623ad689b7f0019bf
Mahendra Bhama <maahibhama@gmail.com>
2019-10-17 00:03:50 +0530

    Added Root view for app.js

--- src/controllers/roots/App/index.js
+++ src/controllers/roots/App/index.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import { Text, View, Button } from 'react-native'
 
+import AppNavigation from '../../../routes/AppNavigation'
+
 import styles from './styles'
 
 export default function App () {
-  const [count, setCount] = useState(0)
 
-  return <View style={styles.container} />
+  return (
+    <View style={styles.container}>
+      <AppNavigation />
+    </View>
+  )
 }
