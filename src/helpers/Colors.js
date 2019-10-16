511244d21188c55bc2eb61de7985bda1cd03d629
Mahendra Bhama <maahibhama@gmail.com>
2019-10-17 00:03:21 +0530

    Create Colors.js

---
+++ src/helpers/Colors.js
@@ -0,0 +1,25 @@
+export const Color = {
+  themeDark: '#4A00E0',
+  themeLight: '#8E2DE2',
+  themeExtraLight: '#E25C58',
+
+  themeBackground: '#FFFFFF',
+  defaultTheme: '#8C8C98',
+  offWhiteBackground: '#F6F6F6',
+
+  themeText: '#707070',
+  darkText: '#323232',
+  lightText: '#A6A6A6',
+  brightText: '#FFFFFF',
+  placeholderText: '#BBBBBB',
+  errorText: '#FD3939',
+  black: '#000000',
+
+  lightBorder: '#BBBBBB',
+  brightBorder: '#DADADA',
+  lightFill: '#BBBBBB',
+  shadow: '#000000',
+
+  actionTint: '#0072BB',
+  transparent: 'transparent'
+}
