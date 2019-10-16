959451959f8a6af2ebc14a9a0f7ca08fcc0bce6c
Mahendra Bhama <maahibhama@gmail.com>
2019-10-17 00:03:05 +0530

    Added Winner View

---
+++ src/controllers/WinnerView/styles.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from 'react-native'
+import { Color } from '../../helpers/Colors';
+import { AppFont } from '../../helpers/Fonts';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import AppStyle from '../../helpers/AppStyle';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  topView:{
+    padding: 10,
+    alignItems: 'flex-end',
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  middleView:{
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottomView:{
+    flex: 1
+  },
+  winnerLogo:{
+    width: 200,
+    height: 200
+  },
+  playerName:{
+    fontSize: 25,
+    fontFamily: AppFont.titleExtraLargeBold.name,
+    fontWeight: AppFont.titleExtraLargeBold.weight,
+    color: 'orange',
+    marginBottom: 20
+  },
+  headingText:{
+    fontSize: 25,
+    fontFamily: AppFont.titleExtraLargeBold.name,
+    fontWeight: AppFont.titleExtraLargeBold.weight,
+    color: Color.darkText,
+    marginBottom: 20
+  },
+  buttonStyles:{
+    width: 200,
+    backgroundColor: 'orange',
+    borderRadius: 5,
+    marginBottom: 15
+  },
+  startButton:{
+    width: '75%',
+    marginTop: 35,
+    backgroundColor: 'lightgreen'
+  },
+  buttonText:{
+    color: Color.brightText
+  },
+  startButtonText:{
+    fontSize: AppFont.titleExtraLargeBold.size,
+    color: Color.brightText
+  }
+})
