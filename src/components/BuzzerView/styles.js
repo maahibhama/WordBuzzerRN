83d6d7dd492ea129cfd4f24e9036e8ce619b0476
maahibhama <maahibhama@gmail.com>
2019-11-03 02:17:52 +0530

    Added some functionality to Buzzer view component

--- src/components/BuzzerView/styles.js
+++ src/components/BuzzerView/styles.js
@@ -1,41 +1,44 @@
-import { StyleSheet } from 'react-native'
+import {StyleSheet} from 'react-native';
 
-import AppStyle from '../../helpers/AppStyle'
-import { AppFont } from '../../helpers/Fonts'
-import { Color } from '../../helpers/Colors'
+import AppStyle from '../../helpers/AppStyle';
+import {AppFont} from '../../helpers/Fonts';
+import {Color} from '../../helpers/Colors';
 
 export default StyleSheet.create({
   containerView: {
     padding: 10,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonContainer: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buzzerStyle: {
     margin: 5,
     height: 50,
-    width: 50
+    width: 50,
   },
   playerName: {
     margin: 5,
     fontSize: AppFont.titleMedium.size,
     fontFamily: AppFont.titleMedium.name,
     fontWeight: AppFont.titleMedium.weight,
-    color: Color.themeBackground
+    color: Color.themeBackground,
   },
   playerScore: {
     margin: 5,
     fontSize: AppFont.titleMedium.size,
     fontFamily: AppFont.titleMedium.name,
     fontWeight: AppFont.titleMedium.weight,
-    color: Color.themeBackground
+    color: Color.themeBackground,
   },
-  checkedImage:{
+  checkedImage: {
     marginHorizontal: 5,
     height: 25,
-    width: 25
-  }
-})
+    width: 25,
+  },
+  disableStyle: {
+    opacity: 0.4,
+  },
+});
