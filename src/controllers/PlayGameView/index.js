57683630b0d601790652cf65559fb1a8762a5946
maahibhama <maahibhama@gmail.com>
2019-11-05 20:45:34 +0530

    Add some functionality to Play Game View

--- src/controllers/PlayGameView/index.js
+++ src/controllers/PlayGameView/index.js
@@ -8,6 +8,7 @@ import styles from './styles';
 import BuzzerView from '../../components/BuzzerView';
 import WordButton from '../../components/WordButton';
 import Routes from '../../routes/Routes';
+import {getRandom, shuffle} from '../../helpers/Utility';
 
 export default function PlayGameView(props) {
   const [is1stSelected, setIs1stSelected] = useState(false);
@@ -15,20 +16,55 @@ export default function PlayGameView(props) {
   const [is3rdSelected, setIs3rdSelected] = useState(false);
   const [is4thSelected, setIs4thSelected] = useState(false);
 
-  const [is1stEnable, setIs1stEnable] = useState(true);
-  const [is2ndEnable, setIs2ndEnable] = useState(true);
-  const [is3rdEnable, setIs3rdEnable] = useState(true);
-  const [is4thEnable, setIs4thEnable] = useState(true);
+  const [is1stEnable, setIs1stEnable] = useState(false);
+  const [is2ndEnable, setIs2ndEnable] = useState(false);
+  const [is3rdEnable, setIs3rdEnable] = useState(false);
+  const [is4thEnable, setIs4thEnable] = useState(false);
 
   const [winnerPlayer, setWinnerPlayer] = useState(null);
   const [showRightWrongSign, setShowRightWrongSign] = useState(false);
   const [rightPlayerList, setRightPlayerList] = useState([]);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [startGameText, setStartGameText] = useState('Start Game');
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
+  const [numberOfQuestion, setNumberOfQuestion] = useState(()=>{
+    return setNumberOfCount()
+  });
+
+  const [randomSelectedWordArray, setRandomSelectedWordArray] = useState([])
+  
+  const [currentSpanishWord, setCurrentSpanishWord] = useState(null);
+  const [currentSWIndex, setCurrentSWIndex] = useState(0);
+
+  const [currentWord, setCurrentWord] = useState(null);
+  const [isTimerStart, setIsTimerStart] = useState(false);
 
   useEffect(() => {
-    return () => {};
-  });
+    const index = currentSWIndex - 1
+    if(currentSWIndex > 0 && randomSelectedWordArray.length > index) {
+      const spanishWord = randomSelectedWordArray[index]
+      setCurrentSpanishWord(spanishWord);
+    }else if(currentSWIndex == 5){
+      setIsTimerStart(false)
+    }else if(currentSWIndex > 0){
+      setIsTimerStart(false)
+    }
+  }, [currentSWIndex]);
+
+  useEffect(() => {
+      const timer = window.setInterval(() => { 
+        if(isTimerStart == true) {
+          console.log(currentSWIndex)
+          getSapnishWord()
+        }else {
+          window.clearInterval(timer);
+        }
+      }, 2000);
+    
+    return () => { // Return callback to run on unmount.
+        window.clearInterval(timer);
+    };
+  }, [isTimerStart]);
 
   const onTouchBuzzerButton = ({buttonId}) => {
     if (buttonId == '1') {
@@ -43,9 +79,17 @@ export default function PlayGameView(props) {
   };
 
   const startButtonAction = () => {
-    setIsGameStarted(true);
-    setStartGameText('Next Word');
-    resetEverything();
+    if (numberOfQuestion > currentQuestionNumber) {
+      setCurrentQuestionNumber(currentQuestionNumber + 1);
+      setIsGameStarted(true);
+      setIsTimerStart(true)
+      setStartGameText('Next Word');
+      resetEverything();
+      setRandomCurrentWord();
+      setCurrentSWIndex(0)
+    } else {
+      props.navigation.navigate(Routes.WinnerView);
+    }
   };
 
   function resetEverything() {
@@ -53,6 +97,7 @@ export default function PlayGameView(props) {
     setWinnerPlayer(null);
     setShowRightWrongSign(false);
     setRightPlayerList([]);
+    allPlayerEnable();
   }
 
   function allPlayerDeseleted() {
@@ -60,6 +105,44 @@ export default function PlayGameView(props) {
     setIs2ndSelected(false);
     setIs3rdSelected(false);
     setIs4thSelected(false);
+  }
+
+  function allPlayerEnable() {
+    setIs1stEnable(true);
+    setIs2ndEnable(true);
+    setIs3rdEnable(true);
+    setIs4thEnable(true);
+  }
+
+  function setNumberOfCount() {
+    const {state} = props.navigation;
+    const {randomElements} = state.params;
+    return randomElements.length
+  }
+
+  function setRandomCurrentWord() {
+    const {state} = props.navigation;
+    const {randomElements, allSpanishWord} = state.params;
+    const currentWord = randomElements[currentQuestionNumber];
+    setNumberOfQuestion(randomElements.length);
+    setSpanishWordArray({
+      currentWord: currentWord,
+      allSpanishWord: allSpanishWord,
+    });
+    setCurrentWord(currentWord);
+  }
+
+  function setSpanishWordArray({currentWord, allSpanishWord}) {
+    const randomSpanishWord = getRandom({arr: allSpanishWord, n: 4});
+    const spanishWordArray = [currentWord.text_spa];
+    const allSapnishWord = shuffle([...randomSpanishWord, ...spanishWordArray]);
+    setRandomSelectedWordArray(allSapnishWord);
+  }
+
+  function getSapnishWord() {
+    if(randomSelectedWordArray.length > 0) {
+      setCurrentSWIndex(currentSWIndex => currentSWIndex + 1);
+    }
   }
 
   return (
@@ -95,15 +178,20 @@ export default function PlayGameView(props) {
           />
         </View>
         <View style={styles.middleView}>
-          {isGameStarted && (
+          {isGameStarted && currentSpanishWord != null && (
             <View style={styles.transalatedTextView}>
-              <Text style={styles.tranaslatedText}>{'Ola'}</Text>
+              <Text style={styles.tranaslatedText}>{currentSpanishWord}</Text>
             </View>
           )}
-          {isGameStarted && <Text style={styles.actualText}>{'Hello'}</Text>}
+          {isGameStarted && currentWord != null && (
+            <Text style={styles.actualText}>{currentWord.text_eng}</Text>
+          )}
           {isGameStarted && (
             <Text style={styles.numberOfWordText}>
-              {'Number of Words: 1/20'}
+              {'Number of Words: ' +
+                currentQuestionNumber +
+                '/' +
+                numberOfQuestion}
             </Text>
           )}
           <WordButton
